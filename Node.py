270c42721b50c3661ff04cc85e800f58976e4d6a
Renan Thiago <renannojosa@gmail.com>
2016-10-17 19:51:32 -0300

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,12 +1,13 @@
 class Node:
     def __init__(self,simbolo):
-        self.simbolo = ''
-        self.frequencia = 0
+        self.__simbolo= ''
+        self.__frequencia = 0
+        self.__esquerda = None
+        self.__direita = None
 
-    def __init__(self,esquerda,direita):
-        self.simbolo = '+'
-        self.esquerda = esquerda
-        self.direita = direita
+    def setChild(self,esquerda,direita):
+        self.__esquerda = esquerda
+        self.__direita = direita
 
     def ehFolha(self):
         return self.esquerda is None and self.direita is None
@@ -15,42 +16,34 @@ class Node:
         return self.frequencia
 
     def getSimbolo(self):
-        return self.simbolo
+        return self.__simbolo
 
     def getEsquerda(self):
-        return self.esquerda
+        return self.__esquerda
 
     def getDireita(self):
-        return self.direita
+        return self.__direita
 
 
-    def incrementa(self):
-        self.frequencia += 1
+    def incrementar(self):
+        self.__frequencia += 1
 
     def __str__(self):
         return ("Char %s : Frequencia %i"%(self.simbolo,self.frequencia))
 
     def __cmp__(self, other):
         # maior, menor ou igual
-        if self.frequencia > other.frequencia:
+        if self.__frequencia > other.frequencia:
             return 1
-        if self.frequencia < other.frequencia:
+        if self.__frequencia < other.frequencia:
             return -1
         else:
             return 0
 
-    def preencheMapa(self, dicioario,caminho):
+    def preencherMapa(self, dicioario,caminho):
         if self.ehFolha():
             dicionario[self.getSimbolo()] = caminho
             return
         self.preencheMapa(dicionario,caminho + '0')
         self.preencheMapa(dicionario,caminho + '1')
-
-
-
-
-
-
-
-
 

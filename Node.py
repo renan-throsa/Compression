ec399f831e0717adea3360ab4bf1287c5172e702
Renan Thiago <renannojosa@gmail.com>
2016-10-17 21:20:15 -0300

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,20 +1,19 @@
 class Node:
-    def __init__(self,simbolo = ''):
+    def __init__(self, simbolo=''):
         self.__simbolo = simbolo
         self.__frequencia = 0
         self.__esquerda = None
         self.__direita = None
 
-
-    def setChild(self,esquerda,direita):
+    def setChild(self, esquerda, direita):
         self.__esquerda = esquerda
         self.__direita = direita
 
     def ehFolha(self):
-        return self.esquerda is None and self.direita is None
+        return self.__esquerda is None and self.__direita is None
 
     def getFrequencia(self):
-        return self.frequencia
+        return self.__frequencia
 
     def getSimbolo(self):
         return self.__simbolo
@@ -25,26 +24,28 @@ class Node:
     def getDireita(self):
         return self.__direita
 
-
     def incrementar(self):
         self.__frequencia += 1
 
     def __str__(self):
-        return ("Char %s : Frequencia %i"%(self.simbolo,self.frequencia))
+        return u'Char {0:s} : Frequencia {1:d}'.format(self.__simbolo, self.__frequencia)
 
-    def __cmp__(self, other):
-        # maior, menor ou igual
-        if self.__frequencia > other.frequencia:
-            return 1
-        if self.__frequencia < other.frequencia:
-            return -1
-        else:
-            return 0
+    def __lt__(self, no):
+        return self.getFrequencia() < no.getFrequencia()
 
-    def preencherMapa(self, dicioario,caminho):
+    def __eq__(self, no):
+        return self.getFrequencia() == no.getFrequencia()
+
+    def __gt__(self, v):
+        return self.getFrequencia() > no.getFrequencia()
+
+    def preencherMapa(self, dicioario, caminho):
+        """
+
+        :type dicioario: map
+        """
         if self.ehFolha():
             dicionario[self.getSimbolo()] = caminho
             return
-        self.preencheMapa(dicionario,caminho + '0')
-        self.preencheMapa(dicionario,caminho + '1')
-
+        self.preencheMapa(dicionario, caminho + '0')
+        self.preencheMapa(dicionario, caminho + '1')
